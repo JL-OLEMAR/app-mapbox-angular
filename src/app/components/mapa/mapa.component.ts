3a405daf6edb2d25b882bb769373c758111f0dc1
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-25 05:49:30 -0500

    Mostrar el mapa de Mapbox

--- src/app/components/mapa/mapa.component.ts
+++ src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import * as mapboxgl from 'mapbox-gl'
 
 @Component({
   selector: 'app-mapa',
@@ -6,8 +7,20 @@ import { Component, OnInit } from '@angular/core'
   styleUrls: ['./mapa.component.css']
 })
 export class MapaComponent implements OnInit {
-  // constructor () { }
+  mapa!: mapboxgl.Map
+  // constructor () {}
 
   ngOnInit (): void {
+    this.crearMapa()
+  }
+
+  crearMapa (): void {
+    (mapboxgl as any).accessToken = 'pk.eyJ1Ijoib2xlbWFyMTk5NiIsImEiOiJja3dlcXFqdXQwN2N2MnFvNHVmZzI1dndsIn0.pm7JqYKifHWCJMMxthZ_pg'
+    this.mapa = new mapboxgl.Map({
+      container: 'mapa',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [-75.75512993582937 , 45.349977429009954],
+      zoom: 15.8
+    })
   }
 }
