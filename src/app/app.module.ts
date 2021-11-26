c703eee46609ff854c70373b75571342b0f5b5b4
JL-OLEMAR <joseluis19963@gmail.com>
2021-11-26 16:44:00 -0500

    Conectar al servidor de sockets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
+import { SocketIoModule } from 'ngx-socket-io'
 
 import { AppComponent } from './app.component'
 import { MapaComponent } from './components/mapa/mapa.component'
+import { environment } from '../environments/environment'
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { MapaComponent } from './components/mapa/mapa.component'
     MapaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(environment.socketConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
